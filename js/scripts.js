59a25464c8b76ce88f78ca964a1ee8ac4bc9ac41
Andrew Peterson and Kayla Ondracek <andrew@>
2018-01-08 10:01:15 -0800

    create html, javascript file with some calculator functions, empty README.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+}
+
+var number3 = parseInt(prompt("Type first number"));
+var number4 = parseInt(prompt("Type second number"));
+
+alert(add(number3, number4));

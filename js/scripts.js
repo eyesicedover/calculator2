d13c7a6b1a461605ef7641ee00b87a54d8982bee
Kayla Ondracek and Stephanie Faber <kayla@>
2018-01-10 10:29:13 -0800

    change prompts to form fields, separate backend and front end logic, add jquery for form handling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+
 $(document).ready(function(){
-
-	var add = function(number1, number2) {
-		return number1 + number2;
-	}
-
-	var number3 = parseInt(prompt("Type first number"));
-	var number4 = parseInt(prompt("Type second number"));
-
-	alert(add(number3, number4));
-
+	$("form#add").submit(function(event) {
+    event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+	  var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+  	$("#output").text(result);
+	});
 });
